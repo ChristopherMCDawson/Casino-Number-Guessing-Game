2a27422bfbbef617d006511cc82ac91c95605e29
Christopher Decariedawson <ChristopherMCDawson@gmail.com>
2024-03-28 13:39:10 -0400

    Add project files.

---
+++ Casino Number Guessing Game.cpp
@@ -0,0 +1,12 @@
+﻿// Casino Number Guessing Game.cpp : Defines the entry point for the application.
+//
+
+#include "Casino Number Guessing Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
